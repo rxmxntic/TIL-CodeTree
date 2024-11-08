5326fbc5e4fc8775a092ec3d25040f8285775acf
최나영 <121682792+rxmxntic@users.noreply.github.com>
2024-11-08 16:31:54 +0900

    Title: 배열 채우기, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241108/배열 채우기/filling-array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            cnt++;
+
+            if (arr[i] == 0) {
+                break;
+            }
+        }
+        for (int i = cnt; i >= 0; i--) {
+            if (arr[i] != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
