3d215e3c01420d0bd8564c4978db3c673027d6a7
최나영 <121682792+rxmxntic@users.noreply.github.com>
2024-12-03 00:18:07 +0900

    Title: 코로나 메뉴얼2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241203/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 카운팅의 배열의 1에는 A인 사람의 수, 2에는 B, 3에는 C, 4에는 D
+        int[] countArr = new int[5];
+        // type num = 분류 번호
+        int typeNum = 0;
+        char s;
+        int t;
+
+        // s와 t를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
+        for (int i = 0; i < 3; i++) {
+            s = sc.next().charAt(0);
+            t = sc.nextInt();
+
+            if (t >= 37 && s == 'Y')
+                typeNum = 1;
+            else if (t >= 37)
+                typeNum = 2;
+            else if (s == 'Y')
+                typeNum = 3;
+            else
+                typeNum = 4;
+            countArr[typeNum]++;
+        }
+
+        // A부터 D까지 나온 횟수를 출력
+        for (int i = 1; i <= 4; i++) {
+            System.out.print(countArr[i] + " ");
+        }
+
+        if (countArr[1] >= 2) {
+            System.out.print("E");
+        }
+    }
+}
